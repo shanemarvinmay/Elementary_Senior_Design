9f2025a2f807b51ad3161ff31829e1739e085b6c
Jspann1 <jazminespann101@gmail.com>
2020-02-24 14:59:22 -0600

    added constraints, need to fix code (error with Parse)

--- CP2_UI/CP2_UI/LogInView.swift
+++ CP2_UI/CP2_UI/LogInView.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2020 Jazmine N Spann. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Parse
+
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet var logInUsernameField: UITextField!
+    @IBOutlet var logInPasswordField: UITextField!
+    @IBAction func `continue`(_ sender: UIButton)
+    {
+    
+    }
+    @IBAction func forgotLogIn(_ sender: UIButton)
+    {
+        
+    }
+    
+    @IBOutlet weak var segmentControls: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logInUsernameField.text=""
+        logInPasswordField.text=""
+    }
+}
+    override func viewDidAppear(_ animated: Bool) {
+      let currentUser = PFUser.current()
+      if currentUser != nil {
+          loadHomeScreen()
+        }
+}
+func displayErrorM(message: String) {
+    let alertView = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+    let OKAction = UIAlertAction(title: "Okay", style: .default) { (action: UIAlertAction) in
+        }
+    alertView.addAction(OKAction)
+    if let presenter = alertView.popoverPresentationController {
+        presenter.sourceView = self.view
+        presenter.sourceRect = self.view.bounds
+    }
+    self.present(alertView, animated: true, completion:nil)
+}
+func loadHomeScreen () {
+    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    let LogInViewController = storyBoard.instantiateViewController(withIdentifier: "LogInViewController") as! LogInViewController
+    self.present(logInViewController, animated: true, completion: nil)
+}
+
