4fbd2b0df6f43e8e812afcddb62f627cb340abd3
Jspann1 <jazminespann101@gmail.com>
2020-02-25 13:00:13 -0600

    added code

--- NEW_UI/NEW_UI/ViewController.swift
+++ NEW_UI/NEW_UI/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class logInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
 }
 
+/*    */
