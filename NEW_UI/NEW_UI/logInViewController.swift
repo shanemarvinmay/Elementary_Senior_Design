f48706844dcb4b24feb42d63abdfa7c861c35ced
Jspann1 <jazminespann101@gmail.com>
2020-02-27 10:58:42 -0600

    added hide keyboard func

--- NEW_UI/NEW_UI/logInViewController.swift
+++ NEW_UI/NEW_UI/logInViewController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import UIKit
+
+
+class logInViewController: UIViewController, UITextFieldDelegate {
+    override func viewDidLoad() {
+               super.viewDidLoad()
+        self.logInUsernameField.delegate = self
+        self.logInPasswordField.delegate = self
+           }
+     @IBOutlet var logInUsernameField: UITextField!
+        @IBOutlet var logInPasswordField: UITextField!
+        @IBAction func `continue`(_ sender: UIButton)
+        {
+        
+        }
+        @IBAction func forgotLogIn(_ sender: UIButton)
+        {
+            
+        }
+        
+        @IBOutlet weak var segmentControls: UISegmentedControl!
+    
+   /* func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+ */
+  
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    func dismissKeyboard () {
+        view.endEditing(true)
+    }
+}
+
+    
